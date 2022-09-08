0811f86a202ebfbf638e3712ee739e55020522ce
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-09-08 23:20:32 +0800

    firebase setup

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { app } from "./firebase/firebase-config";
 
 export default function App() {
+	console.log(app);
 	return (
 		<View style={styles.container}>
 			<Text>Hello world</Text>
